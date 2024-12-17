97645cde273ba474e0d5dddfcb94a15b78b8ccd5
j-balkovec <jakob.balkovec@gmail.com>
2024-12-17 18:13:45 +0100

    made it slower

--- C++/main.cpp
+++ C++/main.cpp
@@ -38,6 +38,8 @@
  * int weightStingyKnapsack(C, I)
 */
 
+// -- find a new way to do this, it's incredibly slow! [BenchmarkFunctions took 81.5683 seconds.]
+
 #include "../Tools/Item.h"
 #include "../Tools/Utility.h"
 #include "../Tools/Logger.h"       //-- <new> --
@@ -54,16 +56,18 @@
 #include <climits>
 #include <cmath>
 #include <functional>
+#include <thread>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+constexpr int NUM_THREADS = 8;
 
-    auto exec_time_logger = getExecTimeLogger();
-    auto main_logger = getMainLogger();
+void initializeLoggers(std::shared_ptr<spdlog::logger>& exec_time_logger, std::shared_ptr<spdlog::logger>& main_logger) {
+    exec_time_logger = getExecTimeLogger();
+    main_logger = getMainLogger();
+    main_logger->info("Main logger created. Using '{}' threads", NUM_THREADS);
+}
 
-    main_logger -> info("Main logger created");
-
-    // Map that stores min execution times
-    std::map<std::string, double> minExecutionTimes = {
+std::map<std::string, double> setupMinExecutionTimes() {
+    return {
         {"<a> Ant Colony Optimization", 0},
         {"<a> Backtracking", 0},
         {"<a> Branch And Bound", 0},
@@ -82,100 +86,181 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
         {"<h> Transitioning Greedy", 0},
         {"<h> Weight Stingy", 0},
     };
+}
 
-    std::vector<Item> items = parseCSVItems(getFilePath(ITEM_1, main_logger), main_logger);
-    int capacity = parseCSVCapacity(CAPACITY.data(), main_logger);
-    size_t item_size = items.size();
-
-
-    // --------- <work_bench> ---------
-
-    // --- <algorithms> --- <START>
-
+AntColony setupColony() {
     constexpr int numAnts = 100;
     constexpr int numIterations = 500;
     constexpr double alpha = 1.0;
     constexpr double beta = 3.0;
     constexpr double evaporationRate = 0.5;
 
-    AntColony colony(numAnts, numIterations, alpha, beta, evaporationRate);
+    return AntColony(numAnts, numIterations, alpha, beta, evaporationRate);
+}
 
-    logSeparator(exec_time_logger, 'A');
+std::vector<Item> parseItemsAndCapacity(std::shared_ptr<spdlog::logger>& main_logger, int& capacity) {
+    std::vector<Item> items = parseCSVItems(getFilePath(ITEM_1, main_logger), main_logger);
+    capacity = parseCSVCapacity(CAPACITY.data(), main_logger);
+    return items;
+}
 
-    bool log_aco = knapsackACOWrapper(colony, capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_dp = knapsackDPWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_memo = knapsackMemoizationWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_annealing = knapsackSimulatedAnnealingWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    //bool log_recursive = knapsackRecursiveWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    //bool log_backtracking = knapsackBacktrackingWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_branch_and_bound = knapsackBranchAndBoundWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+std::pair<bool, bool> executeAlgorithmsAndHeuristics(const std::vector<Item>& items, 
+                                                     int capacity, 
+                                                     std::map<std::string, double>& minExecutionTimes, 
+                                                     std::shared_ptr<spdlog::logger>& exec_time_logger) {
+
+    // Create threads for concurrent execution
+    std::vector<std::thread> threads;
+
+    logSeparator(exec_time_logger, 'A'); // Algorithm separator
+
+    AntColony colony = setupColony();
+
+    // Create booleans to track success
+    bool log_aco = false;
+    bool log_dp = false;
+    bool log_memo = false;
+    bool log_annealing = false;
+    bool log_branch_and_bound = false;
+    bool log_backtracking = false;
+    bool log_recursive = false;
+
+    bool log_deal_stingy = false;
+    bool log_defensive = false;
+    bool log_heavy = false;
+    bool log_limited = false;
+    bool log_max_of_two = false;
+    bool log_scored = false;
+    bool log_sliding_threshold = false;
+    bool log_standard_greedy = false;
+    bool log_transitioning_greedy = false;
+    bool log_weight_stingy = false;
+
+    // Running algorithms in separate threads
+    threads.push_back(std::thread([&]() {
+        log_aco = knapsackACOWrapper(colony, capacity, items, minExecutionTimes, exec_time_logger);
+        log_dp = knapsackDPWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_memo = knapsackMemoizationWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_annealing = knapsackSimulatedAnnealingWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_recursive = knapsackRecursiveWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_backtracking = knapsackBacktrackingWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_branch_and_bound = knapsackBranchAndBoundWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_deal_stingy = knapsackDealStingyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_heavy = knapsackHeavyGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_limited = knapsackLimitedGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_max_of_two = knapsackMaxOfTwoWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_scored = knapsackScoredGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_sliding_threshold = knapsackSlidingThresholdWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_standard_greedy = knapsackStandardGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
+    threads.push_back(std::thread([&]() {
+        log_transitioning_greedy = knapsackTransitioningGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+        log_weight_stingy = knapsackWeightStingyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
+    }));
+
     
-    // --- <algorithms> --- <END>
+    // Wait for all threads to finish
+    for (auto& t : threads) {
+        if (t.joinable()) {
+            t.join(); 
+        }
+    }
 
+    bool logged_algorithms = log_aco && 
+                             log_dp && 
+                             log_memo && 
+                             log_annealing && 
+                             log_branch_and_bound;
+                             //log_recursive && 
+                             //log_backtracking;
+    bool logged_heuristics = log_deal_stingy && 
+                             log_defensive && 
+                             log_heavy && 
+                             log_limited && 
+                             log_max_of_two &&
+                             log_scored && 
+                             log_sliding_threshold && 
+                             log_standard_greedy && 
+                             log_transitioning_greedy &&
+                             log_weight_stingy;
 
-    // --- <heuristics> --- <START>
+    return std::make_pair(logged_algorithms, logged_heuristics);   
+}
 
-    logSeparator(exec_time_logger, 'H');
-    bool log_deal_stingy = knapsackDealStingyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_defensive = knapsackDealStingyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_heavy = knapsackHeavyGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_limited = knapsackLimitedGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_max_of_two = knapsackMaxOfTwoWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_scored = knapsackScoredGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_sliding_threshold = knapsackSlidingThresholdWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_standard_greedy = knapsackStandardGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_transitioning_greedy = knapsackTransitioningGreedyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-    bool log_weight_stingy = knapsackWeightStingyWrapper(capacity, items, minExecutionTimes, exec_time_logger);
-
-    // --- <heuristics> --- <END>   
-    // --------- <work_bench> ---------
-
-    bool logged_algorithms_long_expr = log_aco &&
-                                       log_dp &&
-                                       log_memo &&
-                                       log_annealing &&
-                                       //log_recursive &&
-                                       //log_backtracking &&
-                                       log_branch_and_bound;
-
-    bool logged_heuristics_long_expr = log_deal_stingy &&
-                                       log_defensive &&
-                                       log_limited &&
-                                       log_max_of_two &&
-                                       log_scored &&
-                                       log_sliding_threshold &&
-                                       log_standard_greedy &&
-                                       log_transitioning_greedy &&
-                                       log_weight_stingy;
-
+void logExecutionResults(std::pair<bool, bool> loggedFunction, std::map<std::string, double>& minExecutionTimes) {
     displayMessage("Execution Completed!", SUCCESS); //jank
 
-    // --- <debug only, not meant for production> ---
     displayMessage("-- [min_exec_times] --\n\n", INFO);
 
-    // -- <log to a separate log file + csv format> -- 
-    for(auto key: minExecutionTimes) {
+    // Log to console or a separate log file
+    for (auto key : minExecutionTimes) {
         std::cout << "[name]: " << key.first << ", " << "[min_time]: " << key.second << std::endl;
     }
-    // -- <log to a separate log file + csv format> -- 
 
-    if(logged_algorithms_long_expr) {
+    if (loggedFunction.first) {
         displayMessage("Logged Algorithms.", SUCCESS);
     } else {
         displayMessage("Something went wrong <algorithms>.", ERROR);
     }
 
-    if(logged_heuristics_long_expr) {
+    if (loggedFunction.second) {
         displayMessage("Logged Heuristics.", SUCCESS);
     } else {
         displayMessage("Something went wrong <heuristics>.", ERROR);
     }
 
-    if (logged_algorithms_long_expr && logged_heuristics_long_expr) {
+    if (loggedFunction.first && loggedFunction.second) {
         displayMessage("Logged both. Check logs: [" + std::string(EXEC_TIME_LOG_FILE.data()) + "]", SUCCESS);
     } else {
         displayMessage("Something went wrong <all>.", ERROR);
     }
-    // --- <debug only, not meant for production> ---
+}
+
+void BenchmarkFunctionsNew() {
+    // Initialize loggers
+    std::shared_ptr<spdlog::logger> exec_time_logger, main_logger;
+    initializeLoggers(exec_time_logger, main_logger);
+
+    // Setup execution time map
+    std::map<std::string, double> minExecutionTimes = setupMinExecutionTimes();
+
+    // Parse items and capacity
+    int capacity;
+    std::vector<Item> items = parseItemsAndCapacity(main_logger, capacity);
+
+    // Execute algorithms and heuristics concurrently
+    std::pair<bool, bool> logged = executeAlgorithmsAndHeuristics(items, capacity, minExecutionTimes, exec_time_logger);
+
+    // Log the execution results
+    logExecutionResults(logged, minExecutionTimes);  // Modify based on actual conditions
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    // Measure the execution time of BenchmarkFunctionsNew
+    auto startNew = std::chrono::high_resolution_clock::now();
+    BenchmarkFunctionsNew();
+    auto endNew = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> durationNew = endNew - startNew;
+    std::cout << "BenchmarkFunctionsNew took " << durationNew.count() << " seconds.\n";
+
     return 0;
 }
